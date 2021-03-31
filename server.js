55c7210015560ee0e059f6174f2cba47e0cb2b5c
RedVinck <34350465+RedVinck@users.noreply.github.com>
2021-03-31 10:47:33 +0200

    added authentication from IBM

--- server.js
+++ server.js
@@ -1,125 +1,78 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
+require('dotenv').config();
+const { Issuer } = require('openid-client');
+const express = require('express');
+const session = require('express-session');
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+const app = express();
+const port = 3000;
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+app.use(session({
+  secret: '123456',
+  resave: true,
+  saveUninitialized: true
+}));
 
-if (mongoURL == null) {
-  var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
-  // If using plane old env vars via service discovery
-  if (process.env.DATABASE_SERVICE_NAME) {
-    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
-    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
-    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
-    mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
-    mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
-    mongoUser = process.env[mongoServiceName + '_USER'];
+Issuer.defaultHttpOptions = { timeout: 15000 }
 
-  // If using env vars from secret from service binding  
-  } else if (process.env.database_name) {
-    mongoDatabase = process.env.database_name;
-    mongoPassword = process.env.password;
-    mongoUser = process.env.username;
-    var mongoUriParts = process.env.uri && process.env.uri.split("//");
-    if (mongoUriParts.length == 2) {
-      mongoUriParts = mongoUriParts[1].split(":");
-      if (mongoUriParts && mongoUriParts.length == 2) {
-        mongoHost = mongoUriParts[0];
-        mongoPort = mongoUriParts[1];
-      }
-    }
-  }
+const issuer = new Issuer({
+  issuer: process.env.APPID_OAUTHSERVERURL,
+  authorization_endpoint: process.env.APPID_OAUTHSERVERURL+'/authorization',
+  token_endpoint: process.env.APPID_OAUTHSERVERURL+'/token',
+  userinfo_endpoint: process.env.APPID_OAUTHSERVERURL+'/userinfo',
+  jwks_uri: process.env.APPID_OAUTHSERVERURL+'/publickeys',
+});
+console.log('Issuer %s %O', issuer.issuer, issuer.metadata);
+issuer.defaultHttpOptions = { timeout: 15000 }
 
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-  }
-}
-var db = null,
-    dbDetails = new Object();
-
-var initDb = function(callback) {
-  if (mongoURL == null) return;
-
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
-};
-
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
+const client = new issuer.Client({
+  client_id: process.env.APPID_CLIENTID,
+  client_secret: process.env.APPID_SECRET
 });
 
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
+let authorizationUrl = client.authorizationUrl({
+  redirect_uri: process.env.REDIRECT_URL_CALLBACK,
+  scope: 'openid email profile'
 });
+console.log(authorizationUrl)
 
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
+app.get('/callback', (req, res) => {
+  client.authorizationCallback(process.env.REDIRECT_URL_CALLBACK,
+      req.query, { 'response_type': 'code' })
+      .then(function (tokenSet) {
+        client.userinfo(tokenSet.access_token)
+            .then(function (userinfo) {
+              res.redirect(process.env.REDIRECT_URL_WEB_APP +
+                  '?name=' + userinfo.name +
+                  '&email=' + userinfo.email +
+                  '&id_token=' + tokenSet.id_token +
+                  '&access_token=' + tokenSet.access_token
+              );
+            })
+            .catch(function (error) {
+              console.log(error);
+              res.redirect(process.env.REDIRECT_URL_WEB_APP);
+            })
+      })
+      .catch(function (error) {
+        console.log(error);
+        res.redirect(process.env.REDIRECT_URL_WEB_APP);
+      })
+})
 
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
+app.get('/loginurl', (req, res) => {
+  res.json({ loginUrl: authorizationUrl });
+})
 
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+app.get('/login', (req, res) => {
+  res.redirect(authorizationUrl);
+})
 
-module.exports = app ;
+app.get('/health', (req, res) => {
+  res.send('up');
+})
+
+app.get('/', (req, res) => {
+  res.send('up');
+})
+
+app.listen(3000, () => console.log('Listening on port 3000'))
